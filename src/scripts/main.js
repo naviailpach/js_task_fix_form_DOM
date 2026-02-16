d5ff27c533184db1e22fa22f4f4efc31b4b0e03a
naviailpach <ivan.chaplya13@gmail.com>
2026-02-16 18:24:32 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,30 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('input');
+
+inputs.forEach((input) => {
+  if (!input.id) {
+    input.id = `${input.name}-input`;
+  }
+
+  if (!input.name) {
+    return;
+  }
+
+  if (
+    input.type === 'submit' ||
+    input.type === 'button' ||
+    input.type === 'hidden'
+  ) {
+    return;
+  }
+
+  const label = document.createElement('label');
+  const labelText = input.name.charAt(0).toUpperCase() + input.name.slice(1);
+
+  label.classList.add('field-label');
+  label.htmlFor = input.id;
+  label.textContent = labelText;
+  input.placeholder = labelText;
+  input.parentNode.appendChild(label);
+});
